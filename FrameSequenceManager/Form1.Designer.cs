6f4ea5068bdd3c995886ee19875c94b7185a6542
junt62 <junt62@qq.com>
2025-06-12 12:38:23 +0800

    添加项目文件。

---
+++ FrameSequenceManager/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace FrameSequenceManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            menuStrip1 = new MenuStrip();
+            文件ToolStripMenuItem = new ToolStripMenuItem();
+            导入文件ToolStripMenuItem = new ToolStripMenuItem();
+            编辑ToolStripMenuItem = new ToolStripMenuItem();
+            statusStrip1.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 428);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.TabIndex = 0;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(248, 17);
+            toolStripStatusLabel1.Text = "提示：软件初始化完成，可以开始执行操作。";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { 文件ToolStripMenuItem, 编辑ToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 25);
+            menuStrip1.TabIndex = 3;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            文件ToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { 导入文件ToolStripMenuItem });
+            文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            文件ToolStripMenuItem.Size = new Size(44, 21);
+            文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 导入文件ToolStripMenuItem
+            // 
+            导入文件ToolStripMenuItem.Name = "导入文件ToolStripMenuItem";
+            导入文件ToolStripMenuItem.Size = new Size(180, 22);
+            导入文件ToolStripMenuItem.Text = "导入";
+            // 
+            // 编辑ToolStripMenuItem
+            // 
+            编辑ToolStripMenuItem.Name = "编辑ToolStripMenuItem";
+            编辑ToolStripMenuItem.Size = new Size(44, 21);
+            编辑ToolStripMenuItem.Text = "编辑";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "序列图片管理器";
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem 文件ToolStripMenuItem;
+        private ToolStripMenuItem 导入文件ToolStripMenuItem;
+        private ToolStripMenuItem 编辑ToolStripMenuItem;
+    }
+}
---
+++ FrameSequenceManager/Form1.cs
@@ -0,0 +1,10 @@
+namespace FrameSequenceManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
